c5205bdbd335fd8688107305e36d235a26c4244d
Jet <166585196+yonquejet@users.noreply.github.com>
2024-09-10 08:54:59 +0800

    Add files via upload

---
+++ la_1_Yonque.py
@@ -0,0 +1,6 @@
+#la_1_ClassObjectsAttributes
+class hero():
+    heroName = "Kagura"
+    
+mage = hero()
+print(mage.heroName)
---
+++ la_1_classAndAttribute.py
@@ -0,0 +1,6 @@
+#la_1_classAndAttribute
+class hero():
+    name = "Luo yi"
+    
+mage = hero()
+print(mage.name)
