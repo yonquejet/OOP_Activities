6e427db0458c5132ca8e559c5d58dad626641922
Jet <166585196+yonquejet@users.noreply.github.com>
2024-09-16 15:20:03 +0800

    Add files via upload

---
+++ la_8_Yonque.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title #instantializing, now attributes
+        self.author = author
+        
+libro = Book("Atomic Habits", "James Clear") #object
+new_libro = Book("48 laws of power", "Robert Greene") #object
+print(f"Original values: {libro.title} and {libro.author}")
+
+del libro.author
+#print(f"Original values: {libro.title} and {libro.author}")
+
+libro.author = "James Clear"
+print(f"Original values: {libro.title} and {libro.author}")
+
+print(f"Updated values: {new_libro.title} and {new_libro.author}")
---
+++ la_9_Yonque.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
+        
+    def __str__(self):
+        return (f"{self.brand} is a car brand.")
+        
+    
+kotse = Car("Honda")
+print(kotse.brand)
+print(kotse)
+
+del kotse
+#print(kotse)
+
+
