2a144ef5b9f45075578070c8185b87b2a83853d9
Jet <166585196+yonquejet@users.noreply.github.com>
2024-09-16 16:47:12 +0800

    Add files via upload

---
+++ la_11_Yonque.py
@@ -0,0 +1,16 @@
+#parent class
+class Phone():
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    def describePhone(self):
+        print(f"{self.brand}, {self.model}")
+
+#child class
+class Android(Phone):
+    def __init__(self, brand, model):
+        Phone.__init__(self,brand,model)
+
+smartPhone = Android("Infinix", "Note 11s")
+smartPhone.describePhone()
